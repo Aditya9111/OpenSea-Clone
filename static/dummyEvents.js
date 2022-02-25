31d5c814dc6a5428f483391da91fb0139712948f
Aditya Arya <52771727+Aditya9111@users.noreply.github.com>
2022-02-25 19:02:28 +0530

    upload to github

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 17,
+    from: "0xAditya",
+    to: "0xAdi",
+    date: "2 months ago",
+  },
+  {
+    price: 17,
+    from: "0xAditya",
+    to: "0xAdi",
+    date: "2 months ago",
+  },
+  {
+    price: 17,
+    from: "0xAditya",
+    to: "0xAdi",
+    date: "2 months ago",
+  },
+  {
+    price: 17,
+    from: "0xAditya",
+    to: "0xAdi",
+    date: "2 months ago",
+  },
+];
